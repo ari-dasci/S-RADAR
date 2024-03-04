4ddb603bad7adbc26383d8b1352b6690d9261eb9
Marina Hernandez Bautista marinahbau@ugr.es - Resp. Francisco Javier Melero Rus <mbautista@ngpu>
2024-03-04 13:07:52 +0100

    Almost every TSFEDL model included and tested in test file

--- SADL/time_series/time_series_utils.py
+++ SADL/time_series/time_series_utils.py
@@ -10,28 +10,7 @@ class TimeseriesDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         return (self.X[index:index+self.seq_len,:].permute(1,0), self.X[index+self.seq_len+1,:])
     
-    
-class TSFEDL_TopModule(torch.nn.Module):
-    def __init__(self, in_features=103, out_features=103, npred=1):
-        super(TSFEDL_TopModule, self).__init__()
-        self.npred = npred
-        self.model = torch.nn.Sequential(
-            torch.nn.Dropout(p=0.2),
-            torch.nn.Linear(in_features=in_features, out_features=50),
-            torch.nn.ReLU(),
-            torch.nn.Linear(in_features=50, out_features=npred*out_features)
-        )
-
-    def forward(self, x):
-        out = self.model(x)
-        if len(out.shape)>2:
-            out = out[:, -1, :]
-        if self.npred > 1:
-            # Reshape to (batch_size, npred, out_features)
-            out = out.reshape(out.shape[0], self.npred, -1)
-
-
-class TimeSeriesDatasetNuevo(torch.utils.data.Dataset):
+class TimeSeriesDatasetV2(torch.utils.data.Dataset):
     def __init__(self, data, window_size, forecast_size, stride=1):
         self.data = data
         self.window_size = window_size
@@ -43,4 +22,5 @@ class TimeSeriesDatasetNuevo(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         i = idx * self.stride
-        return (self.data[i:i+self.window_size], self.data[i+self.window_size:i+self.window_size+self.forecast_size])
+        #Get item permuted so the shape of the tensor matches in TSFEDL
+        return (self.data[i:i+self.window_size].permute(1,0), self.data[i+self.window_size:i+self.window_size+self.forecast_size])
