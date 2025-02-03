39c8b71644868c7774f9ac52963ae7609c628d06
Beatriz Bello Garcia <beatrizbellogarcia@gmail.com>
2025-02-03 14:21:02 +0100

    added federated_data and flexanomalies

--- SADL/__init__.py
+++ SADL/__init__.py
@@ -1,8 +1,8 @@
 from . import images
 from . import static_data
 from . import time_series
-
-__all__ = ['images', 'static_data', 'time_series']
+from . import federated_data
+__all__ = ['images', 'static_data', 'time_series','federated_data']
 
 def get_components():
     return {'components': __all__}
